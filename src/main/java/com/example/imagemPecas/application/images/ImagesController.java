8e135bb8834aac2f7adb426f488048a0990a8075
Luiz Lozano <lozano84dev@gmail.com>
2026-08-19 15:03:25 -0300

    passo 26 da aula 4 concluido

--- src/main/java/com/example/imagemPecas/application/images/ImagesController.java
+++ src/main/java/com/example/imagemPecas/application/images/ImagesController.java
@@ -1,15 +1,21 @@
 package com.example.imagemPecas.application.images;
 
+import com.example.imagemPecas.domain.entity.Image;
+import com.example.imagemPecas.domain.enums.ImageExtension;
 import com.example.imagemPecas.domain.service.ImageService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.io.IOException;
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -19,14 +25,27 @@ import java.util.List;
 public class ImagesController {
 
     private final ImageService service;
+    private final ImageMapper mapper;
 
     @PostMapping
     public ResponseEntity save(
             @RequestParam("file") MultipartFile file,
             @RequestParam("name") String name,
             @RequestParam("tags") List<String> tags
-    ){
+    ) throws IOException {
         log.info("Imagem recebida: name: {}, size: {}",file.getOriginalFilename(), file.getSize());
-        return ResponseEntity.ok().build();
+
+        Image image = mapper.mapToImage(file, name, tags);
+        Image savedImage = service.save(image);
+
+        URI imageUri = buildImageURL(savedImage);
+        return ResponseEntity.created(imageUri).build();
+    }
+
+    //localhost:8080/v1/images/xyxyxyxyxyxyxyx
+    private URI buildImageURL(Image image){
+        String imagePath = "/" + image.getId();
+        return ServletUriComponentsBuilder.fromCurrentRequest().path(imagePath).build().toUri();
     }
 }
+
